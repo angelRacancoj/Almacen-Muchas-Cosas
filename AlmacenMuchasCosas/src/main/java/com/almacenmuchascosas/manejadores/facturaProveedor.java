fa6c9bc77b11fb25b9eb239533f2e88408dfa195
Chejohrpp <62493947+Chejohrpp@users.noreply.github.com>
2020-03-22 16:46:23 -0600

    Update facturaProveedor.java

--- AlmacenMuchasCosas/src/main/java/com/almacenmuchascosas/manejadores/facturaProveedor.java
+++ AlmacenMuchasCosas/src/main/java/com/almacenmuchascosas/manejadores/facturaProveedor.java
@@ -14,6 +14,7 @@ import com.almacenmuchascosas.factura.Factura;
  */
 class facturaProveedor extends Factura{
     Proveedor proveedor;
+    Articulo[] articulosTransferidos;
     public Proveedor getProveedor(){ 
         return proveedor;
     }
